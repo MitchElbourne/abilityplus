254824e53894bff80e73bba5e43324b8d8a286b9
Mitch <mkelbourne@gmail.com>
2019-05-14 15:28:48 +0100

    Changes to padding for sections to suit theme, homepage title

--- page-homepage.php
+++ page-homepage.php
@@ -4,9 +4,11 @@ get_header();
 ?>
 
 <main role="main">
-  <section>
+  <section id="introduction">
     <div class="container">
-      <h1>Title</h1>
+      <div class="row">
+        <h1 class="col-12 col-md-7 m-auto">Planning your next career journey?</h1>
+      </div>
     </div>
   </section>
 </main>
