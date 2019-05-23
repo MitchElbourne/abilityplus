1c614240c3b4ba043efc7156640ddfc0423cb97e
Darren Barklie <darrenbarklie@gmail.com>
2019-05-23 14:25:49 +0100

    Blog desktop

--- page-blog.php
+++ page-blog.php
@@ -1,19 +1,127 @@
-<?php 
+<?php
 get_header();
-
 ?>
 
-<main>
-  <section>
+<main id="blog-archive">
+
+
+  <!-- Element: HEADER -->
+  <header>
     <div class="container">
-      <h1>wack</h1>
+      <h1>AbilityPlus Blog</h1>
     </div>
-  </section>
+  </header>
+
+
+
+  <!-- Element: BLOG FILTER -->
+  <div class="blog-filter container">
+    <a href="#" class="active">
+      <img src="<?php echo get_theme_file_uri('/assets/svg/icon-check-active.svg'); ?>" class="icon" />
+      All Articles
+    </a>
+    <a href="#">
+      <img src="<?php echo get_theme_file_uri('/assets/svg/icon-check-inactive.svg'); ?>" class="icon" />
+      Job Seekers
+    </a>
+    <a href="#">
+      <img src="<?php echo get_theme_file_uri('/assets/svg/icon-check-inactive.svg'); ?>" class="icon" />
+      Hiring Managers
+    </a>
+  </div><!--blog-filter-->
+
+
+
+  <!-- Element: BLOG LIST -->
+  <section class="blog-list">
+    <div class="container">
+      <div class="row">
+
+        <div class="col col-12 col-md-4">
+          <div class="blog-post">
+            <a href="#">
+              <div class="blog-thumbnail"></div>
+            </a>
+            <a href="#">
+              <h2 class="h4">I Know Exactly What You Are, Always Alive To The Dark</h2>
+            </a>
+            <p>Requires clamping, yeah. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#" class="arrow">
+              <img src="<?php echo get_theme_file_uri('/assets/svg/icon-arrow-right.svg'); ?>" class="icon" />
+            </a>
+          </div><!--blog-post-->
+        </div><!--col-12-->
+
+
+        <div class="col col-12 col-md-4">
+          <div class="blog-post">
+            <a href="#">
+              <div class="blog-thumbnail"></div>
+            </a>
+            <a href="#">
+              <h2 class="h4">Grounded</h2>
+            </a>
+            <p>Requires clamping, yeah. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#" class="arrow">
+              <img src="<?php echo get_theme_file_uri('/assets/svg/icon-arrow-right.svg'); ?>" class="icon" />
+            </a>
+          </div><!--blog-post-->
+        </div><!--col-12-->
+
+        <div class="col col-12 col-md-4">
+          <div class="blog-post">
+            <a href="#">
+              <div class="blog-thumbnail"></div>
+            </a>
+            <a href="#">
+              <h2 class="h4">Hello Russia, Hello America</h2>
+            </a>
+            <p>Requires clamping, yeah. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#" class="arrow">
+              <img src="<?php echo get_theme_file_uri('/assets/svg/icon-arrow-right.svg'); ?>" class="icon" />
+            </a>
+          </div><!--blog-post-->
+        </div><!--col-12-->
+
+        <div class="col col-12 col-md-4">
+          <div class="blog-post">
+            <a href="#">
+              <div class="blog-thumbnail"></div>
+            </a>
+            <a href="#">
+              <h2 class="h4">Hello Russia, Hello America</h2>
+            </a>
+            <p>Requires clamping, yeah. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#" class="arrow">
+              <img src="<?php echo get_theme_file_uri('/assets/svg/icon-arrow-right.svg'); ?>" class="icon" />
+            </a>
+          </div><!--blog-post-->
+        </div><!--col-12-->
+
+        <div class="col col-12 col-md-4">
+          <div class="blog-post">
+            <a href="#">
+              <div class="blog-thumbnail"></div>
+            </a>
+            <a href="#">
+              <h2 class="h4">Yet Another Challeger to the Crown</h2>
+            </a>
+            <p>Requires clamping, yeah. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#" class="arrow">
+              <img src="<?php echo get_theme_file_uri('/assets/svg/icon-arrow-right.svg'); ?>" class="icon" />
+            </a>
+          </div><!--blog-post-->
+        </div><!--col-12-->
+
+
+
+      </div><!--row-->
+    </div><!--container-->
+  </section><!--blog-list-->
+
 </main>
 
 
 <?php
-
-
 get_footer();
 ?>
